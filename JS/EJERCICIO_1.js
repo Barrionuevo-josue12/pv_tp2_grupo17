fe546bda2e721e27387b3f674d1e1322da2d8a22
Barrionuevo-josue12 <barrionuevojosue21@gmail.com>
2025-04-14 17:35:56 -0300

    He hecho el ejercicio 1 del TP

---
+++ JS/EJERCICIO_1.js
@@ -0,0 +1,7 @@
+/*1.Crear una variable denominada integrantes y asignarle la cadena de texto con los
+nombres de los integrantes del grupo.
+Mostrar por consola el siguiente texto:
+Somos (el valor de la variable) y estamos aprendiendo javascript.
+*/
+let integrantes = "Josue,Aldo,Gonzalo,Dante,Nahuel";
+console.log("somos " + integrantes , " y estamos aprendiendo javascript");
