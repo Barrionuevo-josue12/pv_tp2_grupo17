e24c2b3eeeae788a31eeee73b4e054c4abb0ad5d
Barrionuevo-josue12 <barrionuevojosue21@gmail.com>
2025-04-15 13:47:52 -0300

    He hecho el ejercicio 3 del TP

---
+++ JS/Ejercicio_3.js
@@ -0,0 +1,6 @@
+//3.Calcular y mostrar por consola el área de un círculo cuyo radio = 7,5
+const PI = 3.14159; //defino una constante
+let area;
+let radio = 7.5;
+area = PI*(radio**2);
+console.log("El area de un circulo con un radio de "+radio, " es : ",+area);
