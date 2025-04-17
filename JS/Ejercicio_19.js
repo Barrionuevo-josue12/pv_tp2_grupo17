82f3cc12f51c29c9724f76bd1744c09fa8455737
GonzaTaritolay <gonzalotaritolay94@gmail.com>
2025-04-17 17:18:43 -0300

    separando en dos archivos el ejercicio 19

---
+++ JS/Ejercicio_19.js
@@ -0,0 +1,10 @@
+const boton = document.getElementById("botonEjer19");
+const mostrarDatos=()=> {
+    const nombre = document.querySelector('#nombre').value;
+    const apellido = document.querySelector('#apellido').value;
+    const libreta = document.querySelector('#libreta').value;
+
+    alert(`Los datos ingresados son:\nNombre: ${nombre}\nApellido: ${apellido}\nLibreta Universitaria: ${libreta}`);
+  }
+
+boton.onclick = mostrarDatos;
